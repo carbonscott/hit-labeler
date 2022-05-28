20083d632d0733b2185dd87120804bcdbaf6157e
carbonscott <wangimagine@gmail.com>
2022-05-27 22:22:50 -0700

    Add Psana class.

--- hit_labeler/utils.py
+++ hit_labeler/utils.py
@@ -114,3 +114,40 @@ def downsample(assem, bin_row=2, bin_col=2, mask=None):
     warr[ind]  = downCalib[ind] / downWeight[ind]
 
     return warr
+
+
+
+
+class PsanaImg:
+    """
+    It serves as an image accessing layer based on the data management system
+    psana in LCLS.  
+    """
+
+    def __init__(self, exp, run, mode, detector_name):
+        # Biolerplate code to access an image
+        # Set up data source
+        self.datasource_id = f"exp={exp}:run={run}:{mode}"
+        self.datasource    = psana.DataSource( self.datasource_id )
+        self.run_current   = next(self.datasource.runs())
+        self.timestamps    = self.run_current.times()
+
+        # Set up detector
+        self.detector = psana.Detector(detector_name)
+
+
+    def get(self, event_num, mode = "image"):
+        # Fetch the timestamp according to event number...
+        timestamp = self.timestamps[int(event_num)]
+
+        # Access each event based on timestamp...
+        event = self.run_current.event(timestamp)
+
+        # Only three modes are supported...
+        assert mode in ("raw", "image", "calib"), f"Mode {mode} is not allowed!!!  Only 'raw' or 'image' are supported."
+
+        # Fetch image data based on timestamp from detector...
+        read = { "image" : self.detector.image, }
+        img = read[mode](event)
+
+        return img
