01911553086b1afe9614d1a947737eacbf5cad9f
carbonscott <wangimagine@gmail.com>
2022-07-21 17:36:15 -0700

    Support image mode for cxi (156).

--- hit_labeler/data.py
+++ hit_labeler/data.py
@@ -48,16 +48,76 @@ class DataManager:
 
 
 
+## # This only works for very old dataset
+## class CxiManager(DataManager):
+## 
+##     def __init__(self, config_data):
+##         super().__init__()
+## 
+##         # Imported variables...
+##         self.path_csv = getattr(config_data, 'path_csv', None)
+##         self.drc_root = getattr(config_data, 'drc_root', None)
+##         self.username = getattr(config_data, 'username', None)
+##         self.seed     = getattr(config_data, 'seed'    , None)
+## 
+##         # Internal variables...
+##         self.img_tag_list = []
+##         self.MANAGER = 'cxi'
+## 
+##         set_seed(self.seed)
+## 
+##         self.load_cxi_handler()
+## 
+##         return None
+## 
+## 
+##     def load_cxi_handler(self):
+##         with open(self.path_csv, 'r') as fh: 
+##             lines = csv.reader(fh)
+##             next(lines)
+##             for i, line in enumerate(lines):
+##                 path_cxi = line[0]
+##                 self.img_tag_list.append(path_cxi)
+## 
+##         return None
+## 
+## 
+##     def get_img(self, idx):
+##         fl_xtc   = self.img_tag_list[idx]
+##         path_xtc = os.path.join(self.drc_root, fl_xtc)
+##         with h5py.File(path_xtc, 'r') as fh:
+##             img0 = fh.get('entry_1/data_3/data')[()]
+##             img1 = fh.get('entry_1/data_4/data')[()]
+## 
+##         img = np.concatenate((img0, img1), axis = 0)
+## 
+##         # Save random state...
+##         # Might not be useful for this labeler
+##         if not idx in self.img_state_dict:
+##             self.save_random_state()
+##             self.img_state_dict[idx] = self.state_random
+##         else:
+##             self.state_random = self.img_state_dict[idx]
+##             self.set_random_state()
+## 
+##         img = (img - np.mean(img)) / np.std(img)
+## 
+##         return img
+
+
+
+
 class CxiManager(DataManager):
 
     def __init__(self, config_data):
         super().__init__()
 
         # Imported variables...
-        self.path_csv = getattr(config_data, 'path_csv', None)
-        self.drc_root = getattr(config_data, 'drc_root', None)
-        self.username = getattr(config_data, 'username', None)
-        self.seed     = getattr(config_data, 'seed'    , None)
+        self.path_cxi  = getattr(config_data, 'path_cxi' , None)
+        self.username  = getattr(config_data, 'username' , None)
+        self.seed      = getattr(config_data, 'seed'     , None)
+        self.trans     = getattr(config_data, 'trans'    , None)
+        self.psana_img = getattr(config_data, 'psana_img', None)
 
         # Internal variables...
         self.img_tag_list = []
@@ -71,24 +131,33 @@ class CxiManager(DataManager):
 
 
     def load_cxi_handler(self):
-        with open(self.path_csv, 'r') as fh: 
-            lines = csv.reader(fh)
-            next(lines)
-            for i, line in enumerate(lines):
-                path_cxi = line[0]
-                self.img_tag_list.append(path_cxi)
+        multipanel_list = []
+        with h5py.File(self.path_cxi, 'r') as fh:
+            entry_list = sorted([ item for item in fh.keys() if item.startswith('entry_') ], key = lambda x: int(x.split('_')[1]))
+            for entry in entry_list:
+                entry_value = fh.get(entry)
+                data_list = sorted([ item for item in entry_value.keys() if item.startswith('data_') ], key = lambda x: int(x.split('_')[1]))
+                for data in data_list:
+                    key_data = f"{entry}/{data}/data"
+                    num_multipanel = fh.get(key_data).shape[0]
+                    multipanel_list.extend( ( key_data, i) for i in range(num_multipanel) )
+
+        self.img_tag_list = multipanel_list
 
         return None
 
 
     def get_img(self, idx):
-        fl_xtc   = self.img_tag_list[idx]
-        path_xtc = os.path.join(self.drc_root, fl_xtc)
-        with h5py.File(path_xtc, 'r') as fh:
-            img0 = fh.get('entry_1/data_3/data')[()]
-            img1 = fh.get('entry_1/data_4/data')[()]
+        key_data, idx_data = self.img_tag_list[idx]
 
-        img = np.concatenate((img0, img1), axis = 0)
+        with h5py.File(self.path_cxi, 'r') as fh:
+            multipanel = fh.get(key_data)[idx_data]
+
+        _placeholder_event_num = 0
+        img = self.psana_img.get(_placeholder_event_num, multipanel)
+
+        # Apply any possible transformation...
+        if self.trans is not None: img = self.trans(img)
 
         # Save random state...
         # Might not be useful for this labeler
@@ -112,7 +181,7 @@ class PsanaManager(DataManager):
         super().__init__()
 
         # Imported variables...
-        self.path_csv = getattr(config_data, 'path_csv', None)
+        self.path_cxi = getattr(config_data, 'path_cxi', None)
         self.mode     = getattr(config_data, 'mode'    , None)
         self.detector = getattr(config_data, 'detector', None)
         self.username = getattr(config_data, 'username', None)
@@ -142,7 +211,7 @@ class PsanaManager(DataManager):
 
     def load_imglabel_handler(self):
         # Read csv file of datasets...
-        with open(self.path_csv, 'r') as fh: 
+        with open(self.path_cxi, 'r') as fh: 
             lines = csv.reader(fh)
 
             # Skip the header...
@@ -280,12 +349,13 @@ class SkopiH5Manager(DataManager):
         super().__init__()
 
         # Imported variables...
-        self.path_csv = getattr(config_data, 'path_csv', None)
-        self.drc_root = getattr(config_data, 'drc_root', None)
-        self.username = getattr(config_data, 'username', None)
-        self.panels   = getattr(config_data, 'panels'  , None)
-        self.seed     = getattr(config_data, 'seed'    , None)
-        self.trans    = getattr(config_data, 'trans'   , None)
+        self.path_cxi  = getattr(config_data, 'path_cxi' , None)
+        self.drc_root  = getattr(config_data, 'drc_root' , None)
+        self.username  = getattr(config_data, 'username' , None)
+        self.panels    = getattr(config_data, 'panels'   , None)
+        self.seed      = getattr(config_data, 'seed'     , None)
+        self.trans     = getattr(config_data, 'trans'    , None)
+        self.psana_img = getattr(config_data, 'psana_img', None)
 
         # Internal variables...
         self.img_tag_list = []
@@ -302,7 +372,7 @@ class SkopiH5Manager(DataManager):
 
     def load_skopih5_handler(self):
         counter = 0
-        with open(self.path_csv, 'r') as fh: 
+        with open(self.path_cxi, 'r') as fh: 
             lines = csv.reader(fh)
             next(lines)
             for i, (basename, label, drc) in enumerate(lines):
@@ -338,8 +408,24 @@ class SkopiH5Manager(DataManager):
         return None
 
 
+    def get_img_by_psana(self, idx):
+        path_skopih5, idx_img = self.img_tag_list[idx]
+
+        with h5py.File(path_skopih5, 'r') as fh:
+            multipanel_list = fh.get(self.KEY_TO_IMG)
+            multipanel      = multipanel_list[idx_img]
+
+        _placeholder_event_num = 0
+        img = self.psana_img.get(_placeholder_event_num, multipanel)
+
+        # Apply any possible transformation...
+        if self.trans is not None: img = self.trans(img)
+
+        return img
+
+
     def get_img(self, idx):
-        img = self.get_mosaic(idx)
+        img = self.get_mosaic(idx) if self.psana_img is None else self.get_img_by_psana(idx)
 
         # Save random state...
         # Might not be useful for this labeler
